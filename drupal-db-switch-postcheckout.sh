158ab9f53f221591daac2b6ced57a1ec0944fdd6
Cameron Tod <cameron.tod@gmail.com>
2012-09-10 18:37:37 +0100

    Added detection of drupal root and test for custom settings.php file.

--- drupal-db-switch-postcheckout.sh
+++ drupal-db-switch-postcheckout.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
-branch_name="(unnamed branch)"     # detached HEAD
-branch_name=${branch_name##refs/heads/}
 
-echo $branch_name
+# Default settings.php
+DEFAULT="sites/default/settings.php"
+
+# Get the current git branch.
+BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+BRANCH="(unnamed branch)"     # detached HEAD
+BRANCH=${BRANCH##refs/heads/}
+
+# Return the root of the Drupal installation.
+DRUPAL_ROOT=`drush php-eval 'echo drush_locate_root();'`
+
+
+echo -e "Branch is: $BRANCH\n";
+echo -e "Drupal dir is $DRUPAL_ROOT\n";
+
+DEFAULT="$DRUPAL_ROOT/$DEFAULT"
+CANDIDATE="$DRUPAL_ROOT/sites/$BRANCH/settings.php"
+
+echo -e "Testing for $CANDIDATE\n"
+
+if [ -f $CANDIDATE ]
+then
+  echo "including branch name file $CANDIDATE"
+else
+ echo "including default file"
+fi
